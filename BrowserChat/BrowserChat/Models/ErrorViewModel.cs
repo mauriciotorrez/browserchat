898a7b8dfbddb51a0d003ecb89bd5f8d1504541f
mauriciotorrez <mauricio.torrezm@gmail.com>
2020-01-25 21:24:14 -0400

    Identity Login

---
+++ BrowserChat/BrowserChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrowserChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
